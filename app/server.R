7339c06590f1761932dca688aaae8c0f90c6979b
Yang Cai <yc3404@columbia.edu>
2018-10-07 16:26:58 -0400

    completing recommendation system

--- app/server.R
+++ app/server.R
@@ -1,4 +1,4 @@
-packages.used=c("shiny","ggmap","leaflet","dplyr","shinyBS","plotly","extrafont","grDevices","shinyjs")
+packages.used=c("DT","shiny","ggmap","leaflet","dplyr","shinyBS","plotly","extrafont","grDevices","shinyjs")
 
 # check packages that need to be installed.
 packages.needed=setdiff(packages.used,intersect(installed.packages()[,1],packages.used))
@@ -8,6 +8,7 @@ if(length(packages.needed)>0){
   install.packages(packages.needed, dependencies = TRUE)
 }
 
+library(DT)
 library(shiny)
 library(ggmap)
 library(leaflet)
@@ -17,8 +18,142 @@ library(plotly)
 library(extrafont)
 library(grDevices)
 library(shinyjs)
-library(shiny.router)
+
+
+
+schooldata <- read.csv(file="final3data_with_tuition.csv",stringsAsFactors = FALSE)
+
+#Convert City types into 1 and o (1 for city and 0 for not city)
+school1 <- schooldata %>% mutate(city_nocity=ifelse(schooldata$Citytype=='City',1,0))%>%
+  mutate(c_nc=ifelse(city_nocity==1,"City","Not City"))
+
+#Prepare font list for radar plot
+font1 <- list(family="Montserrat",size=16,color="white")
 
 server <- function(input, output){
   
+  #Recommendation System Plot
+legendtitle_1 <- list(yref='paper',xref="paper",y=1.05,x=1, text="City Radar Chart",showarrow=F)
+legendtitle_2 <- list(yref='paper',xref="paper",y=1.05,x=1, text="Not City Radar Chart",showarrow=F)
+
+sat_score <- reactive({input$satscore/16})
+tuition <- reactive({input$tuition/500})
+crime <- reactive({10000/input$crime})
+ 
+data_radar <- reactive({c(sat_score(),tuition(),crime())})
+  
+  observeEvent(input$city,{
+    if(input$city=="City"){
+  output$radarplot <- renderPlotly({
+    plot_ly(
+     type="scatterpolar",
+     showlegend=T,
+     mode='markers+lines+text',
+     line=list(color="#8D8680",width=2),
+     marker=list(color="#8D8680",size=15,opacity=0.8,symbol="star",line=list(color="#8D8680",width=1)),
+     
+      r=data_radar(),
+      theta = c("SAT score","Tuition","Crime Rate"),
+      fill='toself',fillcolor='rgba(255,0,0,0.5')%>%
+      layout(
+        polar = list(bgcolor='rgba(0,0,0,0.5)',
+          radialaxis = list(
+            linewidth=2,
+            linecolor="white",
+            tickwidth=2,
+            tickcolor="white",
+            showline = T,
+            gridcolor="white",
+            range = c(0,100)
+          )
+      ))%>%
+        layout(plot_bgcolor='#00000000')%>%
+        layout(paper_bgcolor='#00000000')%>%
+        layout(font=font1,annotations=legendtitle_1)
+     
+    
+  })}})
+  
+  observeEvent(input$city,{
+    if(input$city=="Not City"){
+      output$radarplot <- renderPlotly({
+        plot_ly(
+          type="scatterpolar",
+          
+          mode='markers+lines',
+          line=list(color="#8D8680",width=2),
+          marker=list(color="#8D8680",size=15,opacity=0.8,symbol="star",line=list(color="#8D8680",width=1)),
+          r=data_radar(),
+          theta = c("SAT score","Tuition","Crime Rate"),
+          fill='toself',fillcolor='rgba(0,0,255,0.5')%>%
+          layout(
+            polar = list(bgcolor='rgba(0,0,0,0.5)',
+                         radialaxis = list(
+                           linewidth=2,
+                           linecolor="white",
+                           tickwidth=2,
+                           tickcolor="white",
+                           showline = T,
+                           gridcolor="white",
+                           range = c(0,100)
+                         )
+            ))%>%
+          layout(plot_bgcolor='#00000000')%>%
+          layout(paper_bgcolor='#00000000')%>%
+          layout(font=font1,annotations=legendtitle_2)
+        
+        
+      })}})
+  
+###Recommendation data table####
+city1 <- reactive(if(input$city=="City"){city1 <- 1}
+                    else {city1 <- 0})
+satscore1 <- reactive({satscore <- input$satscore})
+tuition1 <- reactive({tuition <- input$tuition})
+crime1 <- reactive({crime <- input$crime})
+
+d1 <- reactive({d1 <- filter(school1, as.numeric(SAT)<= satscore1())})
+d2 <- reactive({d2 <- filter(d1(), as.numeric(CrimeRate) <= crime1())})
+d3 <- reactive({d3 <- filter(d2(),as.numeric(gsub('\\$|,', '', Tuition.and.fees.y)) <= tuition1())})
+d4 <- reactive({ if (city1() == 1){d4 <- filter(d3(),as.numeric(city_nocity)==1)}
+   else{d4 <- filter(d3(),as.numeric(city_nocity)==0)} })
+
+observeEvent(input$getschool,{
+  output$uni <- DT::renderDataTable({
+  
+   school_dt <-
+      subset(
+        d4(),
+        #d4(),
+        select = c(
+          "Name",
+          "SAT",
+          "c_nc",
+          "Tuition.and.fees.y",
+          "CrimeRate"
+          
+        )
+      )
+    
+    colnames(school_dt) <-
+      c(
+        "School Name",
+        "SAT Score",
+        "City/Rural",
+        "Tuition",
+        "Crime Rate (Per 100000 people)"
+        
+      )
+    
+    datatable(
+      school_dt,
+      rownames = F,
+      selection = "single",
+      options = list(order = list(list(0, 'asc'), list(1, "asc")))
+    )  %>%
+      formatCurrency(("Tuition"), digits = 0)
+  }, server = T)
+}) 
+
 }
+
--- app/ui.r
+++ app/ui.r
@@ -1,4 +1,4 @@
-packages.used=c("shiny","ggmap","leaflet","dplyr","shinyBS","plotly","extrafont","grDevices","shinyjs")
+packages.used=c("DT","shiny","ggmap","leaflet","dplyr","shinyBS","plotly","extrafont","grDevices","shinyjs")
 
 # check packages that need to be installed.
 packages.needed=setdiff(packages.used,intersect(installed.packages()[,1],packages.used))
@@ -8,7 +8,7 @@ if(length(packages.needed)>0){
   install.packages(packages.needed, dependencies = TRUE)
 }
 
-
+library(DT)
 library(shiny)
 library(ggmap)
 library(leaflet)
@@ -21,7 +21,7 @@ library(shinyjs)
 library(shinythemes)
 
 
-
+schooldata <- read.csv(file="final3data_with_tuition.csv",stringsAsFactors = FALSE)
 
 
 
@@ -30,33 +30,56 @@ library(shinythemes)
 ui <- navbarPage(theme=shinytheme("lumen"),
   includeCSS("style.css"),
     tabPanel(
+      title="Home",icon=icon("home"),
+      fluidRow(
+        column(width=12,div(style="height:200px;"))),
+      fluidRow(
+        column(width=12,div(style="height:60px;",HTML('<center><img src="sc4.png" width="70"></center>')
+        )
+        )),
+      fluidRow(
+        column(width=4,offset=4,div(style="height:70px;"),h1("NY Schools Hunter",align="center",style="color:white;font-family:Montserrat;")
+        )
+      ),
+      fluidRow(
+        column(width=4,offset=4,div(style="height:100px",p("Our project takes all available data on colleges and universities in New York State and creates a useful 
+                                                           shiny app that allows users to explore and compare schools based on user-specific filtering criteria. 
+                                                           The purpose of our design is to provide users with a 
+                                                           bird's eye view of New York colleges and universities; 
+                                                           allow them to filter, search, and group schools by their preferred criteria; 
+                                                           and further compare two schools on a more micro level.",align="center",style="color:#e6e6e6")))
+        )
+      
+        ),
+  
+    
+    tabPanel(
       title="Maps", icon=icon("map")),
     tabPanel(
       title="Comparison",icon=icon("balance-scale")),
     tabPanel(
-      title="Recommendation System",icon=icon("thumbs-up"))
-  ,
-  fluidRow(
-    column(width=12,div(style="height:200px;"))),
-  fluidRow(
-    column(width=12,div(style="height:60px;",HTML('<center><img src="sc4.png" width="70"></center>')
-                        )
-  )),
-  fluidRow(
-    column(width=4,offset=4,div(style="height:70px;"),h1("NY Schools Hunter",align="center",style="color:white;font-family:Montserrat;")
-           )
-  ),
-  fluidRow(
-    column(width=4,offset=4,div(style="height:100px",p("Our project takes all available data on colleges and universities in New York State and creates a useful 
-                                                       shiny app that allows users to explore and compare schools based on user-specific filtering criteria. 
-                                                       The purpose of our design is to provide users with a 
-                                                       bird's eye view of New York colleges and universities; 
-                                                       allow them to filter, search, and group schools by their preferred criteria; 
-                                                       and further compare two schools on a more micro level.",align="center",style="color:#e6e6e6")))
-  )
- 
-)
-
+      title="Recommendation System",icon=icon("thumbs-up"),
+      
+                 fluidRow(column(width=3,
+              absolutePanel(width=550,
+                wellPanel(
+                  h3("SELECT FILTERS",style="color:black;font-family:Futura Md BT;"),
+              sliderInput("satscore",label=tags$b("SAT Score",style="color:black;font-family:Futura Bk BT;"),min=400, max=1600, value=1000,step=10),
+              
+               selectInput("city",label=tags$b("Located in city/rural",style="color:black;font-family:Futura Bk BT;"),choices=c("City","Not City")),
+              
+              sliderInput("crime",label=tags$b("Crime Rate (Per 100000 people)",style="color:black;font-family:Futura Bk BT;"),min=20,max=1300,value=600),
+              
+                sliderInput("tuition", label=tags$b("Tuition (Per Year)",style="color:black;font-family:Futura Bk BT;"),min=10000,max=60000,value=30000),
+              actionButton("getschool",label="WHAT'S YOUR SCHOOL"), dataTableOutput("uni")),style="opacity:0.8;font-family:Montserrat;"
+              )),
+               
+                 column(width=8,div(style="height:50px"),
+                 plotlyOutput("radarplot"))
+      )))
+      
+  
+  
 
 
   
